8486b9e4f3feb48535ba3b49a95200868152385c
kirill3pt <kirill.dib9@yandex.ru>
2025-11-10 22:55:27 +0300

    начальная реализация интерфейса ILoggable, класса ExpressionEvaluator

--- LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ExpressionEvaluator.cpp
+++ LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ExpressionEvaluator.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include "ExpressionEvaluator.h"
 
+ExpressionEvaluator::ExpressionEvaluator() : n(20) { // конструктор без параметров, 20 значений инициализируется нулями
+	operands = new int[n];
+	for (int i = 0; i < 20; i++) {
+		operands[i] = 0;
+	}
+}
+
+ExpressionEvaluator::ExpressionEvaluator(int count) : n(count) { // конструктор в кач-ве параметров принимает n операндов и инициализирует их нулями
+	operands = new int[n];
+	for (int i = 0; i < n; i++) {
+		operands[i] = 0;
+	}
+}
+
+ExpressionEvaluator::~ExpressionEvaluator() {
+	delete[] operands;
+}
+
 void ExpressionEvaluator::setOperand(size_t pos, double value) {
 
 }
--- LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ExpressionEvaluator.h
+++ LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ExpressionEvaluator.h
@@ -2,28 +2,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "ILoggable.h"
 
-class ExpressionEvaluator {
+class ExpressionEvaluator : public ILoggable { //реализация класса ILoggable
 private:
 	int n; // количество операндов x1, x2, x3 и т.д.
 	int* operands; // массив операндов
 public:
 	virtual double calculate() = 0;
-	ExpressionEvaluator() : n(20) { // конструктор без параметров, 20 значений инициализируется нулями
-		operands = new int[n];
-		for (int i = 0; i < 20; i++) {
-			operands[i] = 0;
-		}
-	}
-	ExpressionEvaluator(int count) : n(count) { // конструктор в кач-ве параметров принимает n операндов и инициализирует их нулями
-		operands = new int[n];
-		for (int i = 0; i < n; i++) {
-			operands[i] = 0;
-		}
-	}
-	virtual ~ExpressionEvaluator() {
-		delete[] operands;
-	}
+	ExpressionEvaluator();
+	ExpressionEvaluator(int count);
+	virtual ~ExpressionEvaluator();
 	void setOperand(size_t pos, double value);
 	void setOperands(double ops[], size_t n);
 };
