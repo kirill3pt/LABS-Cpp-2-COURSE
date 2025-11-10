3d2cf37feeb947d5d1b6f4a99148dce4f28906a8
kirill3pt <kirill.dib9@yandex.ru>
2025-11-10 22:51:10 +0300

    начальная реализация интерфейса ILoggable, класса ExpressionEvaluator

---
+++ LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ExpressionEvaluator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "ExpressionEvaluator.h"
+
+void ExpressionEvaluator::setOperand(size_t pos, double value) {
+
+}
+
+void ExpressionEvaluator::setOperands(double ops[], size_t n) {
+
+}
--- LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ILoggable.h
+++ LAB3. Inheritance, Polymorphism, Interfaces/Berezhetskiy K.T., IVT-2, 2 course, LAB3/ILoggable.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+class ILoggable {
+public:
+	virtual void logToScreen() const = 0;
+	virtual void logToFile(const std::string& filename) const = 0;
+	virtual ~ILoggable() = default;
+};
